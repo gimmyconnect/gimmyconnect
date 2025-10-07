9ee43e39d593f05ae55de13cf0fe49e94ae881a7
gimmyconnect <acceptedX.77@proton.me>
2025-10-07 15:21:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot = telebot.TeleBot("token")
+bot = telebot.TeleBot("8497228841:AAGVTU5PUHaqVcq69c9eqnvpZ1vmfBLNPQk")
 
 @bot.message_handler(func=lambda _: True)
 def reply(message):
